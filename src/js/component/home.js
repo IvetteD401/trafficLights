3d9edc7aed694dc2e7ec01774cc4e7a0b8fb01b8
Ivette Dominguez <ivettedominguez104@gmail.com>
2019-12-17 14:21:30 +0000

    lights flicker

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,9 +6,9 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	const [lightRed, setLight1] = useState("light red off");
-	var myVar = setInterval(myTimer, 1000);
+	var myVar = setInterval(myTimer, 5000);
 	function myTimer() {
-		var d = changeGreen();
+		var d = [changeGreen(), changeRed(), changeYellow()];
 		document.getElementById("auto").innerHTML = d;
 	}
 	const changeRed = () => {
@@ -47,44 +47,15 @@ export function Home() {
 			</div>
 			<div className="btn-group" role="group">
 				<button type="button" className="btn btn-success" id="auto">
-					Auto
-				</button>
-				<button type="button" className="btn btn-danger">
-					Off
+					On
 				</button>
 				<button type="button" className="btn btn-warning">
 					Click
 				</button>
+				<button type="button" className="btn btn-danger">
+					Off
+				</button>
 			</div>
-			{/* Flexbox container for aligning the toasts
-			<div
-				aria-live="polite"
-				aria-atomic="true"
-				className="d-flex justify-content-center align-items-center"
-				style={{ minHeight: "200px" }}>
-				{/* Then put toasts within */}
-			{/* <div
-					className="toast"
-					role="alert"
-					aria-live="assertive"
-					aria-atomic="true">
-					<div className="toast-header">
-						<img src="..." className="rounded mr-2" alt="..." />
-						<strong className="mr-auto">Bootstrap</strong>
-						<small>11 mins ago</small>
-						<button
-							type="button"
-							className="ml-2 mb-1 close"
-							data-dismiss="toast"
-							aria-label="Close">
-							<span aria-hidden="true">×</span>
-						</button>
-					</div>
-					<div className="toast-body">
-						Hello, world! This is a toast message.
-					</div>
-				</div> */}
-			{/* </div>  */}
 		</>
 	);
 }
